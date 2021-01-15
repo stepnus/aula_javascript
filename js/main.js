a7f32d8bf78e5fa515503def54b13795f21d65d1
Raphael Santos <phael.dsantos@gmail.com>
2021-01-15 16:44:01 -0300

    Aplica Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
-alert("meu primeiro js");
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].cor)
+/*
+
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor)
+
+*/
+
+/*
+
+var lista = ["maçã", "pera", "laranja"]
+lista.push("uva");
+//lista.pop();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" "))
+
+*/
+
+/*
+
+var nome = "Raphael";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+//alert("Bem vindo " + nome);
+//alert(frase.replace("Japão", "Brasil"));
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+//console.log(frase.replace("Japão", "Brasil"));
+
+*/
