2cccb58c4b5a6a0427b537c7985770a1aa2977e5
Raphael Santos <phael.dsantos@gmail.com>
2021-01-15 17:17:59 -0300

    Aplicando condicionais, loops e date

--- js/main.js
+++ js/main.js
@@ -1,27 +1,56 @@
+
+var d = new Date()
+alert(d.getDay())
+alert(d.getHours())
+alert(d.getMinutes())
+
+/*
+var count
+for(count = 0; count <= 5; count++) {
+    alert(count)
+}
+*/
+
+/*
+var count = 0
+while (count <= 5) {
+    console.log(count)
+    alert(count)
+    count++
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade?")
+if (idade >= 18) {
+    alert("maior de idade")
+} else {
+    alert("menor de idade")
+}
+*/
+
+/*
 var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
 console.log(frutas);
 alert(frutas[1].cor)
 /*
 
+/*
 var fruta = {nome:"maçã", cor:"vermelha"}
 console.log(fruta.nome);
 alert(fruta.cor)
-
 */
 
 /*
-
 var lista = ["maçã", "pera", "laranja"]
 lista.push("uva");
 //lista.pop();
 console.log(lista);
 console.log(lista.toString());
 console.log(lista.join(" "))
-
 */
 
 /*
-
 var nome = "Raphael";
 var n1 = 5;
 var n2 = 3;
@@ -33,5 +62,4 @@ console.log(nome);
 console.log(n1 * n2);
 console.log(frase.toLowerCase());
 //console.log(frase.replace("Japão", "Brasil"));
-
 */
