956760c651d23423c8dad25cf03e2fdcf6c5d66b
Raphael Santos <phael.dsantos@gmail.com>
2021-01-21 12:29:54 -0300

    Aplicando functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,38 @@
 
+function soma(n1, n2) {
+    return n1 + n2
+}
+
+function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome)
+}
+
+function validaIdade(idade) {
+    
+    var validar
+
+    if(idade >= 18) {
+        validar = true
+    }
+    else {
+        validar = false
+    }
+
+    return validar
+}
+
+var idade = prompt("Qual sua idade")
+console.log(validaIdade(idade))
+
+//alert(setReplace("Vai Japão", "Japão", "Brasil"))
+//alert(soma(5, 10))
+
+/*
 var d = new Date()
 alert(d.getDay())
 alert(d.getHours())
 alert(d.getMinutes())
+*/
 
 /*
 var count
