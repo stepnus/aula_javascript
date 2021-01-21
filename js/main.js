74d8c4ceeb06e4fc279f77db3d16b503e1cd657c
Raphael Santos <phael.dsantos@gmail.com>
2021-01-21 13:36:39 -0300

    Aplicando events

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
 
+function clicou() {
+    console.log(document.getElementById("agradecimento").innerHTML = "<b>Obrigado por Clicar!</b>")
+    //alert("Obrigado por clicar!")
+}
+
+function redirecionar() {
+    window.open("https://www.google.com.br")
+    //window.location.href = "https://www.google.com.br"
+}
+
+function trocar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    elemento.innerHTML = "Obrigado por passar o mouse"
+    //alert("trocar texto")
+}
+
+function voltar(elemento) {
+    elemento.innerHTML = "Passe o mouse aqui"
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+}
+
+function load() {
+    alert("pagina carregada")
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value)
+}
+/*
 function soma(n1, n2) {
     return n1 + n2
 }
+*/
 
+/*
 function setReplace(frase, nome, novo_nome) {
     return frase.replace(nome, novo_nome)
 }
@@ -21,8 +52,10 @@ function validaIdade(idade) {
     return validar
 }
 
+
 var idade = prompt("Qual sua idade")
 console.log(validaIdade(idade))
+*/
 
 //alert(setReplace("Vai Japão", "Japão", "Brasil"))
 //alert(soma(5, 10))
